f79d9987cec10eafc705b39cc998b29b1a482c74
Nangiro <kira.fernando@gmail.com>
2021-01-12 10:46:53 -0300

    Usando dados dinamicos (DONE)

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "bD7bpG-zDJQ",
+        title: "Star Wars Episode I: The Phantom Menace - Trailer",
+        duration: "2:13 min",
+        price: "Free"
+    },
+    {
+        id: "gYbW1F_c9eM",
+        title: "Star Wars Episode II: Attack of the Clones - Trailer",
+        duration: "2:30 min",
+        price: "Free"
+    },
+    {
+        id: "5UnjrG_N8hU",
+        title: "Star Wars Episode III: Revenge of the Sith - Trailer",
+        duration: "2:30 min",
+        price: "Free"
+    },
+    {
+        id: "vZ734NWnAHA",
+        title: "Star Wars Episode IV: A New Hope - Trailer",
+        duration: "1:07 min",
+        price: "Free"
+    },
+    {
+        id: "JNwNXF9Y6kY",
+        title: "Star Wars Episode V: The Empire Strikes Back - Trailer",
+        duration: "2:07 min",
+        price: "Free"
+    },
+    {
+        id: "5UfA_aKBGMc",
+        title: "Star Wars Episode VI: The Return of Jedi - Trailer",
+        duration: "2:11 min",
+        price: "Free"
+    },
+]
--- server.js
+++ server.js
@@ -2,21 +2,35 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express ()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views",{
-    express: server
+    express: server,
+    autoescape: false //Para colocar links no meio de frases
 })
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const about = {
+        avatar_url: "https://www.sideshow.com/storage/product-images/400369/the-child_star-wars_square.jpg",
+        name: "Grogu",
+        role: "Futuro Jedi",
+        description: 'As guardians of peace and justice in the galaxy, they mediate peace negotiations <a href="https://media.giphy.com/media/uNx0YANtETDM6piXju/source.gif" target="_blank">among</a> planets and other factions and, if necessary, use their formidable fighting skills, agility and wisdom to quickly end unrest or neutralize dangerous individuals or threats',
+        links: [
+            { name: "Github", url: "https://github.com/Nangiro" },
+            { name: "Linkedin", url: "https://br.linkedin.com/in/fernandokira" }
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/portifolio", function(req, res){
-    return res.render("portifolio")
+
+    return res.render("portifolio", { items: videos })
 })
 
 server.listen(5000, function() {
